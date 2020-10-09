3b415ec7c5a18a5e0c5e14ee02b472541675511c
annaorujeva <71508284+annaorujeva@users.noreply.github.com>
2020-10-09 15:09:07 +0300

    Create StringNode.java

---
+++ mycollection/StringNode.java
@@ -0,0 +1,11 @@
+package app;
+
+public class StringNode {
+    public StringNode next;
+    public String value;
+
+    public StringNode(String value) {
+        this.value = value;
+        this.next = null;
+    }
+}
